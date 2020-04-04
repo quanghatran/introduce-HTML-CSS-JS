b401ad4ddcddccc65a4a9b54f743f5123533bc51
quanghatran <quanghatran2161998@gmail.com>
2020-04-04 12:10:56 +0700

    update part 2

--- js/main.js
+++ js/main.js
@@ -5,3 +5,37 @@ $(document).ready(function() {
         $("header .list-menu ul").toggleClass("showing");
     });
 });
+
+
+// 
+// $(function() {
+    
+//     // dislay or hide the menu if the user resize the window
+//     $(window).resize(function() {
+//         var wi = $(window).width();
+//         if (wi >= 641) {
+//             $('#topbar-menu').css({'display':'block'});
+//           		  $('#topbar-menu-icon').removeClass('ion-close-round');
+//             $('#topbar-menu-icon').addClass('ion-navicon-round');
+//         }
+//         else {
+//             $('#topbar-menu').css({'display':'none'});
+//             $('#topbar-menu-icon').removeClass('ion-close-round');
+//             $('#topbar-menu-icon').addClass('ion-navicon-round');
+//         }
+//     });
+    
+//     // Change the menu icon, and show or hide the menu
+//     $('#topbar-menu-icon').click(function(){
+//         if ($('#topbar-menu').css('display') == 'none') {
+//             $('#topbar-menu').css({'display':'block'});
+//             $('#topbar-menu-icon').removeClass('ion-navicon-round');
+//             $('#topbar-menu-icon').addClass('ion-close-round');
+//         } 
+//         else {
+//             $('#topbar-menu').css({'display':'none'});
+//             $('#topbar-menu-icon').removeClass('ion-close-round');
+//             $('#topbar-menu-icon').addClass('ion-navicon-round');
+//         }
+//     });
+// });
